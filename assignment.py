f07fe0326c6da07ef6fa8a4ab8198125c5ecf60b
jifkevanderwiel <jifkevanderwiel@gmail.com>
2026-03-03 15:19:04 +0100

    eerste zoektocht naar feature selection

--- assignment.py
+++ assignment.py
@@ -4,7 +4,7 @@
 
 #%% Data loading functions. Uncomment the one you want to use
 #from worcgist.load_data import load_data
-#from worclipo.load_data import load_data
+from worclipo.load_data import load_data
 #from worcliver.load_data import load_data
 #from hn.load_data import load_data
 #from ecg.load_data import load_data
@@ -13,3 +13,17 @@ data = load_data()
 print(f'The number of samples: {len(data.index)}')
 
 print(f'The number of columns: {len(data.columns)}')
+
+## Feature selection
+# Er zijn 3 keer zo veel features als samples. dat is niet handig. dus er moeten er een paar uit
+# stap 1: variantie kijken
+
+variances = data.select_dtypes(include="number").var()
+
+# in sommeige kolommen is de variantie 0, die extraheren
+zero_var_cols = variances[variances == 0].index
+data_drop = data.drop(columns=zero_var_cols)
+variances_drop = variances.drop(columns=zero_var_cols)
+print(data_drop.columns)
+percentage = (variances_drop / variances_drop.sum()) * 100
+print(percentage)
